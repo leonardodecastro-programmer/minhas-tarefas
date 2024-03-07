26d42f785ac9b7a78818e5c0c83ae865696486ef
quickly <leonardo-programmer@proton.me>
2024-03-07 13:16:56 -0300

    Fix: reset itens

--- src/store/reducers/tarefa.ts
+++ src/store/reducers/tarefa.ts
@@ -7,29 +7,7 @@ type TarefasState = {
 }
 
 const initialState: TarefasState = {
-  itens: [
-    {
-      id: 1,
-      descricao: 'Estudar JavaScript revendo o exercício do módulo 7',
-      prioridade: enums.Prioridade.NORMAL,
-      status: enums.Status.CONCLUIDA,
-      titulo: 'Estudar JavaScript'
-    },
-    {
-      id: 2,
-      descricao: 'Estudar material de apoio',
-      prioridade: enums.Prioridade.NORMAL,
-      status: enums.Status.PENDENTE,
-      titulo: 'Estudar TypeScript'
-    },
-    {
-      id: 3,
-      descricao: 'Praticar a construção de uma landing page',
-      prioridade: enums.Prioridade.IMPORTANTE,
-      status: enums.Status.PENDENTE,
-      titulo: 'Estudar Bootstrap'
-    }
-  ]
+  itens: []
 }
 
 const tarefasSlice = createSlice({
